e99443d26ae9c975ac60f51cba77ceb568cbc5c9
Kouhei Watanabe <kouhei.w.56@gmail.com>
2017-10-19 14:55:06 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   # as:でもルーティングできるがhelf_pathにしないとできない
   # get  '/help', to: 'static_pages#help', as: 'helf'
@@ -8,4 +6,6 @@ Rails.application.routes.draw do
   get  '/about', to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 end
